0e3ac085632c379f8ed7641a90c297a6d4a5036e
Demidog03 <olzhasktl362@gmail.com>
2026-01-24 21:01:01 +0500

    1. Переменные и типы данных

---
+++ script.js
@@ -0,0 +1,118 @@
+// console.log("Всем привет!")
+// alert('Толкын') // - простое уведомление
+// prompt("Сколько вы отдыхаете?") // - уведомление с вопросов и ответ как тест
+// confirm("Готовы ли вы поехать в Египет?") // - уведомление с вопросом и ответ только Да или нет
+
+// JS - слабо типизированный
+
+// camelCase
+
+// простые переменные
+// myProject
+// myFirstProject
+
+// классы
+// class Student
+
+// константы
+// const MY_PASSWORD
+
+// var
+
+// var frontender = "Assel" // declaration and initialization of variable -> обьявление и инициализация переменной
+// LEGACY - устаревший
+// console.log(frontender) - должна быть ошибка но из за var тут undefined
+// var frontender // declaration - обьявление
+// frontender = "Altair" // assigning - присвоение
+// console.log(frontender)
+// frontender = "Sagynysh"
+// console.log(frontender)
+
+// let
+// console.log(student) // здесь будет ошибка в отличии от var
+// let student = "Vladimir"
+// student = "Aisultan"
+// console.log(student)
+
+// const
+// const salary = 400000
+// // salary = 500000 // ошибка нельзя менять значение
+// console.log(salary)
+
+
+// ТИПЫ ДАННЫХ
+
+// STRING - строка (текст)
+// console.log('Привет')
+// console.log("Пока")
+// console.log(`Hello`)
+// console.log('Привет Тимур!')
+// console.log("Сегодняя тема - \"Javascript\"")
+
+
+// NUMBER - число
+// console.log(1)
+// console.log(200)
+// console.log(3.14)
+// console.log(200000)
+
+
+
+// BOOLEAN - булевые (истина или ложь) (да или нет)
+// console.log(true)
+// console.log(false)
+
+// const isArmanFrontender = true
+// console.log(isArmanFrontender)
+// const canIDrinkCola = false
+// console.log(canIDrinkCola)
+
+
+// UNDEFINED - неопределенное
+// let name
+// console.log(name)
+
+
+// NULL - нул (ничего)
+// const middleName = null
+// console.log(middleName)
+
+
+// SYMBOL - символ
+// const name1 = Symbol("Kairat")
+// const name2 = Symbol("Kairat")
+// console.log(name1 === name2) // будет false
+
+
+// BIG INT - Big integer (большое число)
+// const number1 = BigInt("9999999999999999999999999999999999999999999999999")
+// console.log(number1)
+
+
+// OBJECT - Обьект
+
+// const name = 'Aisultan'
+// const surname = "Nabiev"
+// const age = 22
+// const isMarried = false
+
+// const person1 = {
+//     name: 'Aisultan',
+//     surname: 'Nabiev',
+//     age: 22,
+//     isMarried: false
+// }
+
+// console.log(person1)
+// console.log(person1.surname)
+
+
+// STRING CONCATINATION - КОНКАТЕНАЦИЯ СТРОК (обьединение строк)
+console.log("Привет" + " " + "Асель")
+
+const reciever = "Альтаир"
+const sender = "Олжас"
+console.log(`С новым годом тебя, ${reciever}! Желаю счастья, здоровья и денег! С уважением, ${sender}.`)
+
+// 1. Вместо строк поставь переменные
+// 2. Вывести результат в консоль
