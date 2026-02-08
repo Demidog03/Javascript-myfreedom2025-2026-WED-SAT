81d2fde2a447bffaca72706f76af63a11c4540d0
Demidog03 <olzhasktl362@gmail.com>
2026-02-08 21:00:21 +0500

    4. Циклы, функции и область видимости (часть 3)

--- lesson4.js
+++ lesson4.js
@@ -1,3 +1,7 @@
+// hoisting
+// var num
+// function sayHi() {}
+
 // for (let i = 0; i < 10; i++) {
 //     console.log(i)
 // }
@@ -138,25 +142,25 @@
 
 
 
-function sumTwoNumbers(num1, num2) {
-    // if (num1 === undefined || num2 === undefined) {
-    if (arguments.length < 2) {
-        console.log('Некоторые аргументы отсутсвуют')
-    }
-    else if (typeof num1 !== 'number' || typeof num2 !== 'number') {
-        console.log('Невалидные параметры')
-    }
-    else {
-        console.log(num1 + num2)
-    }
-}
+// function sumTwoNumbers(num1, num2) {
+//     // if (num1 === undefined || num2 === undefined) {
+//     if (arguments.length < 2) {
+//         console.log('Некоторые аргументы отсутсвуют')
+//     }
+//     else if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+//         console.log('Невалидные параметры')
+//     }
+//     else {
+//         console.log(num1 + num2)
+//     }
+// }
 
-sumTwoNumbers(2, 2)
-sumTwoNumbers(5, 10)
-sumTwoNumbers('5', 5)
-sumTwoNumbers(null, undefined)
-sumTwoNumbers()
-sumTwoNumbers(3)
+// sumTwoNumbers(2, 2)
+// sumTwoNumbers(5, 10)
+// sumTwoNumbers('5', 5)
+// sumTwoNumbers(null, undefined)
+// sumTwoNumbers()
+// sumTwoNumbers(3)
 
 // 1. Нужно проверить переданы ли все аргументы
 // Test cases:
@@ -169,3 +173,199 @@ sumTwoNumbers(3)
 // sumTwoNumbers(true, 'fdsfds') => console.log('Невалидные параметры')
 
 // typeof
+
+
+
+
+
+
+
+
+// Задание 4
+
+// Вывести в консоль квадраты чисел от 0 до 100 (не включая 100) - 
+// т.е. вывести числа 0, 1, 4, 9, 16...
+
+// ** Задавать количество чисел, которые нужно вывести, при помощи функции prompt.
+
+
+// Задание 7
+
+// Переписать задание 4 в виде функции. Т.е.
+// описать функцию, которая выводит квадраты
+// чисел из заданного диапазона (диапазон 
+// будет попадать в функцию как аргументы - min и max).
+
+// function showSquareOfNumbers(min, max) {
+//     if (min >= max) {
+//         console.log('Аргумент min больше или равно аргументу max. Невалидные аргументы!')
+//         return
+//     }
+    
+//     for (let i = min; i < max; i++) {
+//         console.log(i ** 2)
+//     }
+    
+// }
+
+// // showSquareOfNumbers(0, 10)
+// // showSquareOfNumbers(5, 20)
+// showSquareOfNumbers(5, 2)
+// showSquareOfNumbers(5, 5)
+// showSquareOfNumbers(5, 10)
+
+
+
+// RETURN - возврат (возвращать)
+// function sumTwoNumbers(num1, num2) {
+//     // если нет return
+//     // если просто return без значения (ответа)
+//     // return undefined
+//     return num1 + num2
+//     console.log('Hello')
+// }
+
+// let result = sumTwoNumbers(8, 7) // const result = 15
+// console.log(result)
+// result++
+// console.log(result)
+
+// console.log(sumTwoNumbers(5, 5)) // console.log(10)
+
+
+
+
+// Задание 8
+// Написать функцию, которая возвращает строку - случайный цвет в формате rgb.
+// Это будет строка вида "rgb(10,55,250)"
+
+// Здесь у вас будет две функции: одна ваша - getRandomRGB,
+// а вторая - функция получения случайного числа в диапазоне min - max.
+// Вот эта функция (просто скопируйте, она уже готова):
+
+// function getRandomInteger(min, max) {
+//     return Math.floor(Math.random() * (max - min)) + min;
+// }
+
+// getRandomInteger(0, 100) // 98
+
+// Подсказка
+// function hello() {
+//     getRandomInteger()
+// }
+
+// Подсказки: 
+
+// - Каждый цвет - 3 числа, каждое число лежит в диапазоне от 0 до 255. 
+// - Одна функция может вызывать другую функцию
+
+
+// function getRandomInteger(min, max) {
+//     return Math.floor(Math.random() * (max - min)) + min;
+// }
+
+// function generateRandomRgb() {
+//     const r = getRandomInteger(0, 256)
+//     const g = getRandomInteger(0, 256)
+//     const b = getRandomInteger(0, 256)
+
+//     return `rgb(${r},${g},${b})`
+// }
+
+// setInterval(function () {
+//     document.body.style.backgroundColor = generateRandomRgb()
+// }, 1000)
+
+// var и function подлежать hoisting-у (всплытие)
+
+// console.log(num)
+// var num = 5
+
+
+// sayHi()
+
+// function sayHi() {
+//     console.log('Hi')
+// }
+
+
+
+// Функции могут быть перезаписаны
+
+// 1
+// function sayHi() {
+//     console.log('Hi')
+// }
+
+// function sayHi() {
+//     console.log('Hello')
+// }
+
+// sayHi()
+
+// 2
+// function sayHi() {
+//     console.log('Hi')
+// }
+
+// var sayHi = 'Hello'
+
+// sayHi()
+
+
+// Функции ввиде переменных - анонимные и стрелочные функции
+
+// 1
+// const sayHi = function () {
+//     console.log('Hi')
+// }
+
+// // Ошибка
+// const sayHi = 'hi'
+
+// sayHi()
+
+//2
+// const sayHi = () => {
+//     console.log('Hi')
+// }
+
+// sayHi()
+
+
+// Задание 10
+
+// Написать функцию calcPrice, которая рассчитывает и выводит в 
+// консоль стоимость аренды авто на кол-во дней, которое приходит в функцию как параметр.
+
+// День аренды автомобиля стоит $40.
+// При аренде на 7 или больше дней вы получаете общую итоговую скидку $50. 
+// Если вы арендуете авто на 3 или более дней, ваша общая скидка составит $20.
+
+// ИСПОЛЬЗУЙТЕ СТРЕЛОЧНУЮ ИЛИ АНОНИМНУЮ ФУНКЦИЯ
+
+// P.S
+// const sayHi = function () {
+//     console.log('Hi')
+// }
+
+// const sayHi = () => {
+//     console.log('Hi')
+// }
+
+const rentCost = 40
+const calcPrice = (daysForRent) => {
+    if (daysForRent >= 7) {
+        return rentCost * daysForRent - 50
+    }
+    else if (daysForRent >= 3) {
+        return rentCost * daysForRent - 20
+    }
+    else {
+        return rentCost * daysForRent
+    }
+}
+
+console.log(calcPrice(8))
+console.log(calcPrice(5))
+console.log(calcPrice(2))
